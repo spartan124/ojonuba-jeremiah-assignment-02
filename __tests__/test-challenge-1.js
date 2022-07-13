fc4680cebcf7f0ad740b81d4b68d1ebfe44c1363
OjonubaJeremiah <>
2022-07-13 17:04:35 +0000

    completed assignment

--- __tests__/test-challenge-1.js
+++ __tests__/test-challenge-1.js
@@ -4,27 +4,27 @@ const { sumOfNumbers } = assignment;
 const maybe = sumOfNumbers === undefined ? test.skip : test;
 
 maybe('Challenge - 1 : my sumNumbers function can sum an array of numbers', () => {
-    const numbers = [
-        68,
-        -68,
-        27,
-        94,
-        72,
-        -25,
-        -51,
-        32,
-        10,
-        64,
-        -94,
-        4,
-        34,
-        -86,
-        90,
-        81,
-        20,
-        -56,
-        -91,
-        -50
-    ];
-    expect(sumOfNumbers(numbers)).toBe(75);
+  const numbers = [
+    68,
+    -68,
+    27,
+    94,
+    72,
+    -25,
+    -51,
+    32,
+    10,
+    64,
+    -94,
+    4,
+    34,
+    -86,
+    90,
+    81,
+    20,
+    -56,
+    -91,
+    -50
+  ];
+  expect(sumOfNumbers(numbers)).toBe(75);
 });
